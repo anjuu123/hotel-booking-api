73bed4de629c35e8107ec0a5e22255d5468382a1
anju <anjudh5802@gmail.com>
2022-04-28 23:16:23 -0400

    database connection setup

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express from "express";
 import { readdirSync } from "fs";
+import cors from "cors";
+import mongoose from 'mongoose'
+const morgan = require("morgan");
 require('dotenv').config();
+
 
 const app = express();
 
+const URI = process.env.DATABASE;
+// db connection
+mongoose
+.connect(URI, {
+   useNewUrlParser: true, 
+   useUnifiedTopology: true
+})
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log("DB Connection Error: ", err));
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
 
 // route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
