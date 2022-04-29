178a27e7c5c040c56a186711923923cf62802a51
anju <anjudh5802@gmail.com>
2022-04-28 20:10:16 -0400

    server running port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { readdirSync } from "fs";
+require('dotenv').config();
 
 const app = express();
 
@@ -7,5 +8,5 @@ const app = express();
 // route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-
-app.listen(8000,() => console.log('Server is running'));
+const port = process.env.PORT || 8000;
+app.listen(port,() => console.log('Server is running'));
