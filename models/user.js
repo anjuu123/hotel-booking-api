c36aade1115b39bd1f338f172ba23239af330384
anju <anjudh5802@gmail.com>
2022-05-20 16:56:38 -0400

    create user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required: 'name is required'
+    },
+
+    email:{
+        type:String,
+        trim:true,
+        required: 'email is required',
+        unique:true,
+    },
+
+    password:{
+        type:String,
+        required:true,
+        min:6,
+         max:64
+    },
+
+    stripe_account_id:'',
+    stripe_seller:{},
+})
